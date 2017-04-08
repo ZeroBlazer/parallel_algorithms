e3735c78b50736a2645e2803687d0e6e1a3181d3
Elías Puma <elpumitaelias@gmail.com>
2017-04-08 15:08:55 -0500

    Added files for matrices and sorting

--- book_funcs/main.cpp
+++ book_funcs/main.cpp
@@ -3,37 +3,9 @@
 #include "trapezoidal.h"
 #include "distribution.h"
 
-enum executer {
-    trapezoid,
-    distribut,
-    matrix_ml,
-    sorting
-};
-
-int main(int argc, char** argv) {
-    executer exec =
-        // trapezoid;
-        distribut;
-        // matrix_ml;
-        // sorting;
-
-    switch (exec) {
-    case trapezoid:
-        trapezoidal();
-        break;
-    case distribut:
-        distribution(100);
-        break;
-    case matrix_ml:
-        trapezoidal();
-        break;
-    case sorting:
-        trapezoidal();
-        break;
-    default:
-        printf("Nothing to execute :(");
-        break;
-    }
+int main(int argc, char** argv) { 
+    // trapezoidal();
+    distribution(100);
     
     return 0;
 }
