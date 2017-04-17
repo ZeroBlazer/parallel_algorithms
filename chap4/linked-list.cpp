bb1d0657b99051208f3e569f490824f6e3a80c3f
Elías Puma <elpumitaelias@gmail.com>
2017-04-17 05:41:47 -0500

    Some code for linked list

--- chap4/linked-list.cpp
+++ chap4/linked-list.cpp
@@ -1,67 +1,111 @@
 #include "linked-list.h"
 
+#define ONEMUTEX
+// #define MUTEXPERNODE
+// #define RWLOCKS
+
 struct list_node_s {
     int data;
     struct list_node_s* next;
+#ifdef MUTEXPERNODE
+    pthread_mutex_t mutex;
+#endif
 };
 
-int Member(int value, struct list_node_s *head_p) {
-	struct list_node_s *curr_p = head_p;
+struct list_node_s *head_p = NULL;
 
-	while (curr_p != NULL && curr_p->data < value)
-		curr_p = curr_p->next;
+void linked_list_operations() {
 
-	if (curr_p == NULL || curr_p->data > value) {
-		return 0;
-	} else {
-		return 1;
-	}
 }
 
-int Insert(int value, struct list_node_s **head_pp) {
-	struct list_node_s *curr_p = *head_pp;
-	struct list_node_s *pred_p = NULL;
-	struct list_node_s *temp_p;
+#ifdef ONEMUTEX
+int Member(int value) {
+    struct list_node_s* curr_p = head_p;
 
-	while (curr_p != NULL && curr_p->data < value) {
-		pred_p = curr_p;
-		curr_p = curr_p->next;
-	}
+    while(curr_p != NULL && curr_p->data < value)
+        curr_p = curr_p->next;
 
-	if (curr_p == NULL || curr_p->data > value) {
-		temp_p = malloc(sizeof(struct list_node_s));
-		temp_p->data = value;
-		temp_p->next = curr_p;
-		if (pred_p == NULL)
-			*head_pp = temp_p;
-		else
-			pred_p->next = temp_p;
-		return 1;
-	} else {
-		return 0;
-	}
+    if (curr_p == NULL || curr_p->data > value) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
-int Delete(int value, struct list_node_s **head_pp) {
-	struct list_node_s *curr_p = *head_pp;
-	struct list_node_s *pred_p = NULL;
+int Insert(int value, struct list_node_s** head_p) {
+    struct list_node_s* curr_p = *head_p;
+    struct list_node_s* pred_p = NULL;
+    struct list_node_s* temp_p;
 
-	while (curr_p != NULL && curr_p->data < value) {
-		pred_p = curr_p;
-		curr_p = curr_p->next;
-	}
+    while (curr_p != NULL && curr_p->data < value) {
+        pred_p = curr_p;
+        curr_p = curr_p->next;
+    }
 
-	if (curr_p != NULL && curr_p->data == value) {
-		if (pred_p == NULL) {
-			*head_pp = curr_p->next;
-			free(curr_p);
-		} else {
-			pred_p->next = curr_p->next;
-			free(curr_p);
-		}
-		return 1;
-	} else {
-		return 0;
-	}
+    if (curr_p == NULL || curr_p->data > value) {
+        temp_p = (list_node_s*) malloc(sizeof(struct list_node_s));
+        temp_p->data = value;
+        temp_p->next = curr_p;
+        if (pred_p == NULL) /* New first node */
+            *head_p = temp_p;
+        else
+            pred_p->next = temp_p;
+        return 1;
+    } else { /* Value already in list */
+        return 0;
+    }
 }
 
+int Delete(int value, struct list_node_s** head_p) {
+    struct list_node_s* curr_p = *head_p;
+    struct list_node_s* pred_p = NULL;
+
+    while (curr_p != NULL && curr_p->data < value) {
+        pred_p = curr_p;
+        curr_p = curr_p->next;
+    }
+
+    if (curr_p != NULL && curr_p->data == value) {
+        if (pred_p == NULL) { /* Deleting first node in list */
+            *head_p = curr_p->next;
+            free(curr_p);
+        } else {
+            pred_p->next = curr_p->next;
+            free(curr_p);
+        }
+        return 1;
+    } else { /* Value isn’t in list */
+        return 0;
+    }
+}
+#endif
+
+// #ifdef MUTEXPERNODE
+// int Member(int value) {
+//     struct list_node_s* temp_p;
+ 
+//     pthread_mutex_lock(&head_p_mutex);
+//     temp_p = head_p;
+//     while(temp_p != NULL && temp_p->data < value) {
+//         if(temp_p->next != NULL)
+//             pthread_mutex_lock(&(temp_p->next->mutex));
+//         if (temp_p == head_p)
+//             pthread_mutex_unlock(&head_p_mutex);
+//         pthread_mutex_unlock(&(temp_p->mutex));
+//         temp_p = temp_p->next;
+//     }
+ 
+//     if(temp_p == NULL || temp_p->data > value) {
+//         if (temp_p == head_p)
+//             pthread_mutex_unlock(&head_p_mutex);
+//         if (temp_p != NULL)
+//             pthread_mutex_unlock(&(temp_p->mutex));
+//         return 0;
+//     } else {
+//         if (temp_p == head_p)
+//             pthread_mutex_unlock(&head_p_mutex);
+//         pthread_mutex_unlock(&(temp_p->mutex));
+//         return 1;
+//     }
+// }
+// #endif
