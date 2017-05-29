33da1ecf42474a4856548e6ae56db6d6307ea617
Elías Puma <elpumitaelias@gmail.com>
2017-05-29 10:21:09 -0500

    class avance chap05

--- chap05/exerc01.c
+++ chap05/exerc01.c
@@ -7,6 +7,19 @@ void Hello(void);
 int main(int argc, char* argv[]) {
     int thread_count = strtol(argv[1], NULL, 10);
 
+    int data_count = 20;
+    float data[] = {1.3,2.9,0.4,0.3,1.3,4.4,1.7,0.4,3.2,0.3,4.9,2.4,3.1,4.4,3.9,0.4,4.2,4.5,4.9,0.9};
+    int min_meas,
+        max_meas,
+        bin_count = ;
+    float bin_maxes[data_count];
+
+    for(int i = 0; i < n; i++) {
+        if(data[i] < min_m)
+            min_m = data[i];
+        if(data[i] > bin_max)
+    }
+
 #pragma omp parallel num_threads(thread_count)
     Hello();
 
