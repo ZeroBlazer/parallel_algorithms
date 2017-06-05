4d5e4708ff423ec2168168e5a9f1643a50279339
Elías Puma <elpumitaelias@gmail.com>
2017-06-04 19:55:51 -0500

    Added Count Sort

--- chap05/exerc03.c
+++ chap05/exerc03.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <omp.h>
 
+void Count sort(int a[], int n) {
+    int i, j, count;
+    int∗ temp = malloc(n∗sizeof(int));
+    
+    for (i = 0; i < n; i++) {
+        count = 0;
+        for (j = 0; j < n; j++)
+            if (a[j] < a[i])
+                count++;
+            else if (a[j] == a[i] && j < i)
+                count++;
+        temp[count] = a[i];
+    }
+
+    memcpy(a, temp, n∗sizeof(int));
+    free(temp);
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         printf("Especify number of iterations");
