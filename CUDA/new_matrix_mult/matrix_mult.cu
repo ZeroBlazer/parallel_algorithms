6b55874b7e99a478d4696ed91f3ee0b7906679d3
Elías Puma <elpumitaelias@gmail.com>
2017-07-12 11:18:27 -0500

    new commit

--- CUDA/new_matrix_mult/matrix_mult.cu
+++ CUDA/new_matrix_mult/matrix_mult.cu
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-#define MAX 200;
+#define MAX 200
 #define TILE_WIDTH 32
 
 __global__
@@ -29,7 +29,7 @@ void print_matrix(float* M, int N) {
 }
 
 __global__
-void MatrixMulKernel(float* M, float* N, float* P,int Width) {
+void MatrixMulKernel(float* M, float* N, float* P, int Width) {
     __shared__ float ds_M[TILE_WIDTH][TILE_WIDTH];
     __shared__ float ds_N[TILE_WIDTH][TILE_WIDTH];
     __shared__ float ds_NN[TILE_WIDTH][TILE_WIDTH];
@@ -45,44 +45,43 @@ void MatrixMulKernel(float* M, float* N, float* P,int Width) {
     float Pvalue = 0; float Pvalue2 = 0;
     // Loop over the M and N tiles required to compute the P element
     if((Row < Width) && (Col < Width)){
-      for (int ph = 0; ph < Width/TILE_WIDTH; ++ph){
+        for (int ph = 0; ph < Width / TILE_WIDTH; ++ph){
 
-        // Collaborative loading of M and N tiles into shared memory
-        ds_M[ty][tx] = M[Row * Width + ph * TILE_WIDTH + tx];    // ph = tile index
-        ds_N[ty][tx] = N[(ph * TILE_WIDTH + ty) * Width + Col];
-        ds_NN[ty][tx] = N[(ph * TILE_WIDTH + ty) * Width +(Col + TILE_WIDTH)];
-        __syncthreads();
+            // Collaborative loading of M and N tiles into shared memory
+            ds_M[ty][tx] = M[Row * Width + ph * TILE_WIDTH + tx];    // ph = tile index
+            ds_N[ty][tx] = N[(ph * TILE_WIDTH + ty) * Width + Col];
+            ds_NN[ty][tx] = N[(ph * TILE_WIDTH + ty) * Width +(Col + TILE_WIDTH)];
+            __syncthreads();
 
-        for (int i = 0; i < TILE_WIDTH; ++i) {
-            Pvalue += ds_M[ty][i] * ds_N[i][tx];
-            Pvalue2 += ds_M[ty][i] * ds_NN[i][tx];
+            for (int i = 0; i < TILE_WIDTH; ++i) {
+                Pvalue += ds_M[ty][i] * ds_N[i][tx];
+                Pvalue2 += ds_M[ty][i] * ds_NN[i][tx];
+            }
+
+            __syncthreads();
         }
 
-        __syncthreads();
-      }
-
-      P[Row * Width + Col] = Pvalue;
-      P[(Row * Width + Col) + TILE_WIDTH] = Pvalue2;
+        P[Row * Width + Col] = Pvalue;
+        P[(Row * Width + Col) + TILE_WIDTH] = Pvalue2;
     }
 }
 
-void Mul_tiled(float *A, float *B, float *C, int N) {
-    long long size = sizeof(float) * N * N;
+void shared_matrix_mult(float *A, float *B, float *C, int N) {
     float *d_A,
           *d_B,
           *d_C;
 
-    cudaMalloc((void **) &d_A, size);
-    cudaMalloc((void **) &d_B, size);
-    cudaMalloc((void **) &d_C, size);
+    cudaMalloc(&d_A, N * N * sizeof(float));
+    cudaMalloc(&d_B, N * N * sizeof(float));
+    cudaMalloc(&d_C, N * N * sizeof(float));
 /*******************************************************/
     dim3 threadsPerBlock(N, N);
     dim3 blocksPerGrid(1, 1);
-    if (N*N > 512) {
-        threadsPerBlock.x = 512;
-        threadsPerBlock.y = 512;
-        blocksPerGrid.x = ceil(double(N)/double(threadsPerBlock.x));
-        blocksPerGrid.y = ceil(double(N)/double(threadsPerBlock.y));
+    if (N * N > 1024) {
+        threadsPerBlock.x = 1024;
+        threadsPerBlock.y = 1024;
+        blocksPerGrid.x = ceil(double(N) / double(threadsPerBlock.x));
+        blocksPerGrid.y = ceil(double(N) / double(threadsPerBlock.y));
     }
 /****************FILLING RANDOM MATRIX******************/    
     rand_matrix<<<blocksPerGrid, threadsPerBlock>>>(d_A, N);
@@ -96,21 +95,17 @@ void Mul_tiled(float *A, float *B, float *C, int N) {
 	cudaEventCreate(&start);
     cudaEventRecord(start, 0);
 
-    MatrixMulKernel<<<dimGrid,dimBlock>>>(d_A, d_B, d_C, N);
+    MatrixMulKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, N);
     
     cudaEventCreate(&stop);
 	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop);
-	cudaEventElapsedTime(&elapsedTime, start,stop);
+	cudaEventElapsedTime(&elapsedTime, start, stop);
 
     cudaMemcpy(C, d_C, N * N * sizeof(float), cudaMemcpyDeviceToHost);
-    // print_matrix(C, N);
+    print_matrix(C, N);
     printf("N: %d\n", N);
     printf("SM Runtime : %f ms\n", elapsedTime);
-    
-    // Clean up:
-    // cudaEventDestroy(start);
-    // cudaEventDestroy(stop);
     
     cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
     
@@ -135,7 +130,7 @@ int main(int argv, char ** argc) {
         B[i] = 3;
     }
 
-    Mul_tiled(A, B, C, N);
+    shared_matrix_mult(A, B, C, N);
     // print_matrix(C, N);
     
     free(A);
